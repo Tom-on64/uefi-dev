62e4e8d4f3103719705a9c43ff54384f6ac70485
Tom-on64 <kubecekt@gmail.com>
2024-10-11 21:20:40 +0200

    Implemented MBR and GDT partition stuff

--- img-create/src/lib.h
+++ img-create/src/lib.h
@@ -1,15 +1,33 @@
 #ifndef MBR_H
 #define MBR_H
 
+#include <stdint.h>
+// #include <uchar.h> I don't have this on MacOS :/
+typedef uint_least16_t char16_t;
+
 /* Constants */
 #define MEGABYTE    (1024*1024)
 #define LBA_SIZE    512
 #define ESP_SIZE    (33*MEGABYTE)
 #define DATA_SIZE   (MEGABYTE)
-#define IMG_SIZE    (ESP_SIZE + DATA_SIZE + MEGABYTE)
+#define IMG_SIZE    (ESP_SIZE + DATA_SIZE + (MEGABYTE*2 + (LBA_SIZE * 67)))
+
+#define ALIGN_LBA   (MEGABYTE / LBA_SIZE)
+
+#define ESP_GUID    ((Guid){ 0xc12a7328, 0xf81f, 0x11d2, 0xba, 0x4b, { 0x00, 0xa0, 0xc9, 0x3e, 0xc9, 0x3b } })
+#define DATA_GUID   ((Guid){ 0xEBD0A0A2, 0xB9E5, 0x4433, 0x87, 0xC0, { 0x68, 0xB6, 0xB7, 0x26, 0x99, 0xC7 } })
+
+#define GPT_TABLE_ENTRY_SIZE    128
+#define GPT_TABLE_ENTRY_COUNT   128
+#define GPT_TABLE_SIZE  (128*128)   // Minimum size
 
 /* Macros */
-#define BYTES2SECTORS(_b)  ( ((_b) / LBA_SIZE) + ((_b) % LBA_SIZE ? 1 : 0) )
+#define BYTES2LBAS(_b)  ( ((_b) / LBA_SIZE) + ((_b) % LBA_SIZE ? 1 : 0) )
+#define NEXT_ALIGNED_LBA(_lba) ( (_lba) - ((_lba) % ALIGN_LBA) + ALIGN_LBA )
+
+/* More Constants */
+#define ESP_LBA     ALIGN_LBA
+#define DATA_LBA    NEXT_ALIGNED_LBA(ESP_LBA + BYTES2LBAS(ESP_SIZE))
 
 /* Typedefs */
 typedef struct {
@@ -28,6 +46,43 @@ typedef struct {
     MbrPartition partition[4];
     uint16_t bootSignature;
 } __attribute__ ((packed)) MasterBootRecord;
+
+typedef struct {
+    uint32_t timeLow;
+    uint16_t timeMid;
+    uint16_t timeHighAndVer;
+    uint8_t clkSeqHighAndRes;
+    uint8_t clkSeqLow;
+    uint8_t node[6];
+} __attribute__ ((packed)) Guid;
+
+typedef struct {
+    uint8_t signature[8];
+    uint32_t revision;
+    uint32_t headerSize;
+    uint32_t headerCrc32;
+    uint32_t _reserved1;
+    uint64_t myLba;
+    uint64_t altLba;
+    uint64_t firstUsableLba;
+    uint64_t lastUsableLba;
+    Guid diskGuid;
+    uint64_t partitionTableLba;
+    uint32_t entryCount;
+    uint32_t entrySize;
+    uint32_t partitionTableCrc32;
+
+    uint8_t _reserved2[512-92];
+} __attribute__ ((packed)) GptHeader;
+
+typedef struct {
+    Guid partitionTypeGuid;
+    Guid uniqueGuid;
+    uint64_t startLba;
+    uint64_t endLba;
+    uint64_t attributes;
+    char16_t name[36];  // UTF-16 For some reason
+} __attribute__ ((packed)) GptPartitionEntry;
 
 #endif
 
--- img-create/src/writegpt.c
+++ img-create/src/writegpt.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <time.h>
 #include "lib.h"
 
 char* imgname = "test.img";
 
+// CRC32
+uint32_t crcTable[256];
+void createCrc32Table();
+uint32_t calcCrc32(void* buf, int32_t len);
+
+int padLbaSize(FILE* image);
+Guid newGuid();
 int writeMbr(FILE* image);
+int writeGpts(FILE* image);
 
 int main(int argc, char** argv) {
+    if (argc > 1) { imgname = argv[1]; }
+
     FILE* image = fopen(imgname, "wb+");
+
+    // Seed RNG
+    srand(time(NULL));
 
     if (image == NULL) {
         fprintf(stderr, "%s: Could not open %s\n", argv[0], imgname);
         return EXIT_FAILURE;
     }
 
+    // Write protective MBR
     if (writeMbr(image) != 0) {
         fprintf(stderr, "%s: Could not write MBR in %s\n", argv[0], imgname);
+        return EXIT_FAILURE;
+    }
+
+    if (writeGpts(image) != 0) {
+        fprintf(stderr, "%s: Could not write GPT tables and headers to %s", argv[0], imgname);
         return EXIT_FAILURE;
     }
 
@@ -25,9 +45,81 @@ int main(int argc, char** argv) {
     return EXIT_SUCCESS;
 }
 
+// Source: https://www.w3.org/TR/png/#D-CRCAppendix
+// Slight modification like using fixed size types and identifier changes
+void createCrc32Table() { // Tbh i have no fucking clue how this works
+    uint32_t c;
+    int32_t n, k;
+
+    for (n = 0; n < 256; n++) {
+        c = (uint32_t) n;
+        for (k = 0; k < 8; k++) {
+            if (c & 1) c = 0xedb88320L ^ (c >> 1);
+            else c = c >> 1;
+        }
+        crcTable[n] = c;
+    }
+}
+
+uint32_t calcCrc32(void* buf, int32_t len) {
+    static int crcTableComputed = 0;
+
+    uint8_t* bufp = buf;
+    uint32_t c = 0xffffffffL;
+    int32_t n;
+
+    if (!crcTableComputed) {
+        createCrc32Table();
+        crcTableComputed = 1;
+    }
+    for (n = 0; n < len; n++) c = crcTable[(c ^ bufp[n]) & 0xff] ^ (c >> 8);
+
+    return c ^ 0xffffffffL;
+}
+
+int padLbaSize(FILE* image) {
+    uint8_t zeroSector[512];
+
+    for (size_t i = 0; i < (LBA_SIZE - sizeof(zeroSector)) / 512; i++) {
+        if (fwrite(zeroSector, sizeof(zeroSector), 1, image) != 1) { return 1; }
+    }
+
+    return 0;
+}
+
+Guid newGuid() {
+    uint8_t randArr[16];
+
+    for (uint8_t i = 0; i < sizeof(randArr); i++) {
+        randArr[i] = rand() % (UINT8_MAX + 1);
+    }
+    
+    Guid res = {
+        .timeLow = *(uint32_t*)&randArr[0],
+        .timeMid = *(uint16_t*)&randArr[4],
+        .timeHighAndVer = *(uint16_t*)&randArr[6],
+        .clkSeqHighAndRes = randArr[8],
+        .clkSeqLow = randArr[9],
+        .node = { randArr[10], randArr[11], randArr[12], randArr[13], randArr[14], randArr[15] },
+    };
+
+    // Version bits (V4)
+    res.timeHighAndVer &= ~(1 << 15);
+    res.timeHighAndVer |= (1 << 14);
+    res.timeHighAndVer &= ~(1 << 13);
+    res.timeHighAndVer &= ~(1 << 12);
+
+    // Variant bits
+    res.clkSeqHighAndRes |= (1 << 7);
+    res.clkSeqHighAndRes |= (1 << 6);
+    res.clkSeqHighAndRes &= ~(1 << 5);
+
+    return res;
+}
+
 int writeMbr(FILE* image) {
-    uint64_t imageSizeSectors = BYTES2SECTORS(IMG_SIZE);
-    if (imageSizeSectors > 0xFFFFFFFF) imageSizeSectors = 0x100000000;
+    uint64_t imageSizeLbas = BYTES2LBAS(IMG_SIZE);
+    if (imageSizeLbas > 0xFFFFFFFF) imageSizeLbas = 0x100000000;
 
     MasterBootRecord mbr = { 0 };
     mbr.partition[0] = (MbrPartition) {
@@ -36,11 +128,79 @@ int writeMbr(FILE* image) {
         .osType = 0xee,     // Protective GPT
         .endChs = { 0xff, 0xff, 0xff },
         .startLba = 1,
-        .sizeLba = imageSizeSectors-1,
+        .sizeLba = imageSizeLbas-1,
     };
     mbr.bootSignature = 0xaa55; 
 
     if (fwrite(&mbr, sizeof(mbr), 1, image) != 1) { return 1; }
+
+    return 0;
+}
+
+int writeGpts(FILE* image) {
+    GptHeader primaryGpt = {
+        .signature = { "EFI PART" },
+        .revision = 0x00010000, // V1.0
+        .headerSize = 92,
+        .headerCrc32 = 0,   // To be calculated
+        .myLba = 1,
+        .altLba = BYTES2LBAS(IMG_SIZE) - 1,
+        .firstUsableLba = 1 + 1 + 32,   // MBR + GPT + Primary GPT table
+        .lastUsableLba = BYTES2LBAS(IMG_SIZE) - 1 - 32 - 1, // Second GPT header + table
+        .diskGuid = newGuid(),
+        .partitionTableLba = 2,
+        .entryCount = 128,
+        .entrySize = 128,
+        .partitionTableCrc32 = 0,   // To be calculated
+    };
+
+    GptPartitionEntry gptTable[GPT_TABLE_ENTRY_COUNT] = {
+        // EFI System Partition
+        {
+            .partitionTypeGuid = ESP_GUID,
+            .uniqueGuid = newGuid(),
+            .startLba = ESP_LBA,
+            .endLba = ESP_LBA + BYTES2LBAS(ESP_SIZE),
+            .attributes = 0,
+            .name = u"EFI SYSTEM",
+        },
+        // Basic Data Partition
+        {
+            .partitionTypeGuid = DATA_GUID,
+            .uniqueGuid = newGuid(),
+            .startLba = DATA_LBA,
+            .endLba = DATA_LBA + BYTES2LBAS(DATA_SIZE),
+            .attributes = 0,
+            .name = u"BASIC DATA",
+        },
+    };
+    
+    // Calc primary CRC32 values
+    primaryGpt.partitionTableCrc32 = calcCrc32(gptTable, sizeof(gptTable));
+    primaryGpt.headerCrc32 = calcCrc32(&primaryGpt, primaryGpt.headerSize);
+
+    // Write primary GPT and GPT Table
+    if (fwrite(&primaryGpt, sizeof(primaryGpt), 1, image) != 1) { return 0; }
+    if (padLbaSize(image) != 0) { return 0; }
+    if (fwrite(gptTable, sizeof(gptTable), 1, image) != 1) { return 0; }
+
+    // Setup secondary (backup) GPT
+    GptHeader secondaryGpt = primaryGpt;
+    secondaryGpt.partitionTableCrc32 = 0;
+    secondaryGpt.headerCrc32 = 0;
+    secondaryGpt.myLba = primaryGpt.altLba;
+    secondaryGpt.altLba = primaryGpt.myLba;
+    secondaryGpt.partitionTableLba = BYTES2LBAS(IMG_SIZE) - 1 - 32;
+
+    // Calc secondary CRC32 values
+    secondaryGpt.partitionTableCrc32 = calcCrc32(gptTable, sizeof(gptTable));
+    secondaryGpt.headerCrc32 = calcCrc32(&secondaryGpt, secondaryGpt.headerSize);
+
+    // Write secondary GPT and GPT table
+    fseek(image, secondaryGpt.partitionTableLba * LBA_SIZE, SEEK_SET);
+    if (fwrite(gptTable, sizeof(gptTable), 1, image) != 1) { return 0; }
+    if (fwrite(&secondaryGpt, sizeof(secondaryGpt), 1, image) != 1) { return 0; }
+    if (padLbaSize(image) != 0) { return 0; }
 
     return 0;
 }
